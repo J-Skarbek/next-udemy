15609d4eacd62e53c8f38473df90076478212764
J-Skarbek <john@snarkmediaagency.com>
2024-04-25 14:21:39 -0500

    Add params object

--- app/app/blog/[slug]/page.js
+++ app/app/blog/[slug]/page.js
@@ -1,7 +1,11 @@
-export default function Post() {
+export default function Post({ params }) {
+  //params pulls in an object of key/value pairs from the URL params -- just like in the
+  //react spa tutorial
+
   return (
     <>
       <h1>This is a type of blog</h1>
+      <p>{params.slug}</p>
     </>
   )
 }
