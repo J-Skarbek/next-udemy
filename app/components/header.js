3dbc7267b7c45d994e2f33e464921caa50bf3490
J-Skarbek <john@snarkmediaagency.com>
2024-04-26 15:48:02 -0500

    More work on header

--- app/components/header.js
+++ app/components/header.js
@@ -1,23 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
+// import logoImg from '@/public/vercel.svg';
 
 export default function Header() {
   return (
     <header className="flex justify-center items-center align-center">
       <div className="logo-container">
-        <Image 
-          src="/vercel.svg"
-          alt="Vercel Logo"
-          className="dark:invert"
-          width={100}
-          height={24}
-          priority
-        />
+        <Link href="/">
+          {/* <img src={logoImg.src} alt="Our Logo"/> */}
+          <Image 
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className="dark:invert"
+            width={100}
+            height={24}
+            priority
+          />
+        </Link>
       </div>
-      <nav className="main-nav flex items-center align center justify-between gap-8 mx-8">
-        <Link href="/meals">Meals</Link>
-        <Link href="/community">Community</Link>
-        <Link href="/about">About</Link>
+      <nav className="main-nav flex items-center align center justify-between mx-8">
+        <ul className="flex items-center align center justify-between gap-8">
+          <li><Link href="/meals">Meals</Link></li>
+          <li><Link href="/community">Community</Link></li>
+          <li><Link href="/about">About</Link></li>
+        </ul>
       </nav>
     </header>
   )
