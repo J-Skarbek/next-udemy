1e3f79c20ce88430e9af1c3f17c1820bdb3000c0
J-Skarbek <john@snarkmediaagency.com>
2024-04-26 15:34:46 -0500

    Rework the header componenets

--- app/components/header.js
+++ app/components/header.js
@@ -1,8 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <>
-      <span>This is my non-page header componenet</span>
-      <p>It can be imported!</p>
-    </>
+    <header className="flex justify-center items-center align-center">
+      <div className="logo-container">
+        <Image 
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={100}
+          height={24}
+          priority
+        />
+      </div>
+      <nav className="main-nav flex items-center align center justify-between gap-8 mx-8">
+        <Link href="/meals">Meals</Link>
+        <Link href="/community">Community</Link>
+        <Link href="/about">About</Link>
+      </nav>
+    </header>
   )
 }
