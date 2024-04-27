c0c1450c51638eecb6ee532bda7c07e95d037fcd
J-Skarbek <john@snarkmediaagency.com>
2024-04-27 15:01:00 -0500

    Add dummy content to the homepage

--- app/app/page.js
+++ app/app/page.js
@@ -1,6 +1,5 @@
 import Link from "next/link"
 
-
 export default function Home() {
     
   return (
@@ -21,11 +20,21 @@ export default function Home() {
         </div>       
       </header>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <h1>This is the homepage</h1> 
-        </div>     
+        <section className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm flex flex-col">
+          <h2 className="font-bold text-4xl my-4">This is the homepage</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quae id repellat quam autem magni! Nihil, omnis quia alias illum aperiam nam consequuntur explicabo sapiente voluptatibus hic officiis suscipit dignissimos. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem assumenda nihil, temporibus illum magnam expedita possimus, earum laudantium accusamus minima voluptate perspiciatis et rerum. Ut ea eius corrupti sit libero.
+          Quas, cum, dolorum provident voluptate itaque iste fugiat est porro qui corporis ut exercitationem ducimus. Hic commodi iusto nihil inventore repellendus nesciunt sed quod necessitatibus eligendi! Sequi, suscipit. Reiciendis, qui.
+          Dolore quam rem cum provident, exercitationem perspiciatis. Ullam quo nam amet quos quasi magnam, laudantium vel. Aliquam perspiciatis possimus ratione neque enim, explicabo nihil deserunt ex dolorem id mollitia nisi!
+          Vitae voluptates, provident nam, eum itaque dolor laudantium voluptatum quam, placeat nobis minus. Delectus sint saepe aliquid veritatis repellat adipisci? Doloremque fuga voluptatum, eaque reprehenderit saepe eius neque a? Quaerat?</p> 
+        </section>
+        <section className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm flex flex-col">
+          <h2 className="font-bold text-4xl my-4">Section 2 - blah blah blah</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quae id repellat quam autem magni! Nihil, omnis quia alias illum aperiam nam consequuntur explicabo sapiente voluptatibus hic officiis suscipit dignissimos. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem assumenda nihil, temporibus illum magnam expedita possimus, earum laudantium accusamus minima voluptate perspiciatis et rerum. Ut ea eius corrupti sit libero.
+          Quas, cum, dolorum provident voluptate itaque iste fugiat est porro qui corporis ut exercitationem ducimus. Hic commodi iusto nihil inventore repellendus nesciunt sed quod necessitatibus eligendi! Sequi, suscipit. Reiciendis, qui.
+          Dolore quam rem cum provident, exercitationem perspiciatis. Ullam quo nam amet quos quasi magnam, laudantium vel. Aliquam perspiciatis possimus ratione neque enim, explicabo nihil deserunt ex dolorem id mollitia nisi!
+          Vitae voluptates, provident nam, eum itaque dolor laudantium voluptatum quam, placeat nobis minus. Delectus sint saepe aliquid veritatis repellat adipisci? Doloremque fuga voluptatum, eaque reprehenderit saepe eius neque a? Quaerat?</p> 
+        </section>   
       </main>
     </>
-    
   );
 }
