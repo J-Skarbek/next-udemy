e2167ef97cd70617ddfc11cc1adeeeadd9bf0c61
J-Skarbek <john@snarkmediaagency.com>
2024-05-01 14:14:40 -0500

    Start on meals details page + add styles module

--- app/app/meals/[slug]/page.js
+++ app/app/meals/[slug]/page.js
@@ -1,9 +1,36 @@
+import Image from 'next/image';
+import classes from './page.module.css';
+
+
 export default function MealDetails({ params }) {
   return (
-    <main>
-      <h1>
-        This page is for {params.slug}
-      </h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill alt='helo' />
+
+        </div>
+        <div className={classes.headerText}>
+          <h1>TITLE</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${'EMAIL'}`}>NAME</a>
+          </p>
+          <p className={classes.summary}></p>
+        </div>
+
+      </header>
+        <main>
+        <h1>
+          This page is for {params.slug}
+        </h1>
+        <p 
+          className={classes.instructions} 
+          dangerouslySetInnerHTML={{
+            __html: '...'
+          }}
+        >
+        </p>
+      </main>
+    </>
   )
 }
