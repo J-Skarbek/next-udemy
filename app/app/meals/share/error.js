c0e679fa89c85722ed3c1dcdb26c00d9e63d4ebf
J-Skarbek <john@snarkmediaagency.com>
2024-05-02 12:00:39 -0500

    Create specific error page for meals/share pathing

---
+++ app/app/meals/share/error.js
@@ -0,0 +1,15 @@
+'use client';
+
+export default function Error() {
+  return (
+    <main>
+      <h1>An error occurred</h1>
+      <p>
+        Form submission fiels were of invalid types.
+      </p>
+      <p>
+        Failed to create meal.
+      </p>
+    </main>
+  )
+}
