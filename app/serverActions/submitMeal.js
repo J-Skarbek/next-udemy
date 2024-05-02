b17c3159934ac7b6d7717fcf5342824b71a54f59
J-Skarbek <john@snarkmediaagency.com>
2024-05-02 11:51:30 -0500

    Set up basic SS form validation

--- app/serverActions/submitMeal.js
+++ app/serverActions/submitMeal.js
@@ -3,6 +3,10 @@
 import { postMeal } from "@/db/meals";
 import { redirect } from "next/navigation";
 
+function isInvalidText(text) {
+  return !text || text.trim() === '';
+}
+
 export default async function submitMeal(formData) {
   // Can declare a function to run only on server, but must add
   // async keyword in addition to the directive below
@@ -13,7 +17,20 @@ export default async function submitMeal(formData) {
     image: formData.get('image'),
     creator: formData.get('name'),
     creator_email: formData.get('email')
+  };
+
+  if (
+    isInvalidText(meal.title) || 
+    isInvalidText(meal.summary) || 
+    isInvalidText(meal.instructions) || 
+    isInvalidText(meal.creator) || 
+    isInvalidText(meal.creator_email) || meal.creator_email.includes('@') || 
+    !meal.image || meal.image.size === 0
+  ) {
+    throw new Error('Invalid Input!')
+
   }
+
   await postMeal(meal);
   redirect('/meals');
 }
