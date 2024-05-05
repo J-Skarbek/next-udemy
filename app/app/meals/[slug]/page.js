f915c49d33f412594bb01e4bdea884f626284b2b
J-Skarbek <john@snarkmediaagency.com>
2024-05-05 12:03:18 -0500

    Add dynamic metadata function

--- app/app/meals/[slug]/page.js
+++ app/app/meals/[slug]/page.js
@@ -3,6 +3,23 @@ import { notFound } from 'next/navigation';
 import { getMeal } from '@/db/meals';
 import classes from './page.module.css';
 
+
+//This function works like static metadata function but must be structred and 
+//named like this so next.js knows to get the metadata for the page
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.slug);
+  //Should add this check to redirect to the correct 404 page
+  //if the page slug does not exist
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary
+  }
+};
+
 export default function MealDetails({ params }) {
 
   const meal = getMeal(params.slug);
